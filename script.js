57dbc760275e648821d30c97a071795f52a68f2f
Adithya V <venkateshadithya007@gmail.com>
2023-07-19 18:45:27 +0530

    basic js variables added

---
+++ script.js
@@ -0,0 +1,9 @@
+// to access elements by attribute, you must use square brackets [] inside the quotes ' '
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextButton = document.querySelector('[data-previous-operand]');
+const currentOperandTextButton = document.querySelector('[data-current-operand]');
