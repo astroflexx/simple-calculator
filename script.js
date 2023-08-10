47b9b3cee79457da2355cfa9cb7ec9226dff02bd
Adithya V <venkateshadithya007@gmail.com>
2023-08-10 20:17:30 +0530

    implemented the compute function

--- script.js
+++ script.js
@@ -21,22 +21,54 @@ class Calculator {
     }
 
     chooseOperation(operation) {
-
+        if (this.currentOperand === "") return;
+        if (this.previousOperand !== "") {
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = "";
     }
 
     compute() {
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
 
+        if (isNaN(prev) || isNaN(current)) return;
+
+        switch (this.operation) {
+            case '+':
+                computation = prev + current;
+                break;
+            case '-':
+                computation = prev - current;
+                break;
+            case '*':
+                computation = prev * current;
+                break;
+            case '÷':
+                computation = prev / current;
+                break;
+            default:
+                return;
+        }
+
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = "";
     }
 
     updateDisplay() {
-        this.currentOperandTextElement.textContent = this.currentOperand; 
+        this.currentOperandTextElement.textContent = this.currentOperand;
+        this.previousOperandTextElement.textContent = this.previousOperand;
     }
 }
 
 // to access elements by attribute, you must use square brackets [] inside the quotes ' '
 
 const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operation]');
+const operationButtons = document.querySelectorAll('[data-attribute]');
 const equalsButton = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
@@ -59,7 +91,10 @@ operationButtons.forEach(button => {
     })
 })
 
-
+equalsButton.addEventListener("click", button => {
+    calculator.compute();
+    calculator.updateDisplay();
+})
 
 
 
